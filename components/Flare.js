66fe6ff7fc001a7c0d00a83beed26e6e133e7b0c
David Smythe <smythe.david@gmail.com>
2020-07-28 13:30:59 +0100

    Index - Hero
    Implement hero design

---
+++ components/Flare.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Flare = () => {
+    return (
+        <div className="flare"></div>
+    );
+};
+
+export default Flare;
---
+++ components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className={"sunset"}>
+            <div className={"sunset_sun"}/>
+            <div className={"sunset_tagline"}>
+                <div className="sunset_intro">
+                    <h1>David Smythe, Web Developer</h1>
+                    <h3>Experienced web developer practiced in bespoke ecommerce and
+                        CMS driven websites</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
